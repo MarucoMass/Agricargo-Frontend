7bb830a7a71b47dedf739ee0a43de3903f560683
Francisco Palena <palenafrancisco@gmail.com>
2024-11-15 21:10:23 -0300

    .

--- src/components/confimationModal/ConfirmationModal.jsx
+++ src/components/confimationModal/ConfirmationModal.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaExclamationTriangle } from "react-icons/fa";
 
-const ConfirmationModal = ({ isOpen, onClose, onConfirm, message}) => {
+const ConfirmationModal = ({ isOpen, onClose, onConfirm, message }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   if (!isOpen) return null;
+
+  const handleConfirm = async () => {
+    setIsLoading(true);
+    await onConfirm();
+    setIsLoading(false);
+  };
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
@@ -12,18 +20,32 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm, message}) => {
           <h2 className="text-xl font-semibold text-black">Confirmar eliminación</h2>
         </div>
         <p className="text-center mb-6 text-black">{message}</p>
-        <div className="flex justify-around">
+        <div className="flex justify-around items-center">
           <button
             onClick={onClose}
-            className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400"
+            disabled={isLoading}
+            className={`px-4 py-2 rounded-md ${
+              isLoading
+                ? "bg-gray-300 cursor-not-allowed"
+                : "bg-gray-300 hover:bg-gray-400"
+            }`}
           >
             Cancelar
           </button>
           <button
-            onClick={onConfirm}
-            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+            onClick={handleConfirm}
+            disabled={isLoading}
+            className={`px-4 py-2 rounded-md text-white ${
+              isLoading
+                ? "bg-red-300 cursor-not-allowed"
+                : "bg-red-500 hover:bg-red-600"
+            }`}
           >
-            Eliminar
+            {isLoading ? (
+              <div className="w-5 h-5 border-2 border-t-2 border-white rounded-full animate-spin"></div>
+            ) : (
+              "Eliminar"
+            )}
           </button>
         </div>
       </div>
